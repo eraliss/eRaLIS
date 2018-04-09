9b2ad3b157c0fdafe9a180a6134b47e6aefa93c0
Tek Nath <nathtek136@gmail.com>
2018-04-09 20:32:07 +0600

    'l'

--- app/src/main/java/com/eralis/MainActivity.java
+++ app/src/main/java/com/eralis/MainActivity.java
@@ -1,13 +1,29 @@
 package com.eralis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login=(Button) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent myIntent=new Intent(MainActivity.this,HomeScreen.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
